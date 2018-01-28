634783da463fed2b10e02f794f495097d87f0626
Matt Payne <Payne@MattPayne.org>
2018-01-27 22:32:10 -0600

    Using fixture for better readablity.

--- cc/test_covered_call.py
+++ cc/test_covered_call.py
@@ -1,8 +1,16 @@
 import pytest
 from covered_call import CoveredCall
 
-def test_example_two():
+@pytest.fixture
+def covered_call():
     cc = CoveredCall("LVS", 73.82, 77.44, 0.41, '2/23/2018', 2)
-    assert cc.immediate_gain() == 73.00
-    cc.fee = cc.fee - 1
-    assert cc.immediate_gain() == 74.00
+    return cc
+
+def test_immediate_gain(covered_call):
+    assert covered_call.immediate_gain() == 73.00
+
+def test_lower_fee_more_gain(covered_call):
+    covered_call.fee = covered_call.fee - 1
+    assert covered_call.immediate_gain() == 74.00
+
+
