256a7b6bbaca6d3aff35a795eb3428888ec274b1
Matt Payne <Payne@MattPayne.org>
2018-01-27 23:01:31 -0600

    Capped gain demostrated.

--- cc/covered_call.py
+++ cc/covered_call.py
@@ -1,15 +1,18 @@
 class CoveredCall(object):
 
-    def __init__(self, symbol, basis, current_price, bid_price, expiration_date, contracts):
+    def __init__(self, symbol, basis, current_price, bid_price, expiration_date, strike_price, contracts):
         self.symbol = symbol
         self.basis = basis
         self.current_price = current_price
         self.bid_price = bid_price
         self.expiration_date = expiration_date
+        self.strike_price = strike_price
         self.contracts = contracts
         self.fee = 7.0
-        self.commission = 2.0;
+        self.commission = 1.0 * self.contracts;
 
     def immediate_gain(self):
         return (self.bid_price) * 100 * self.contracts - self.fee - self.commission
 
+    def capped_gain(self):
+        return (self.strike_price - self.basis) * 100 * self.contracts - self.fee
--- cc/test_covered_call.py
+++ cc/test_covered_call.py
@@ -3,7 +3,7 @@ from covered_call import CoveredCall
 
 @pytest.fixture
 def covered_call():
-    cc = CoveredCall("LVS", 73.82, 77.44, 0.41, '2/23/2018', 2)
+    cc = CoveredCall("LVS", 73.82, 77.44, 0.41, '2/23/2018', 85.00, 2)
     return cc
 
 def test_immediate_gain(covered_call):
@@ -12,5 +12,8 @@ def test_immediate_gain(covered_call):
 def test_lower_fee_more_gain(covered_call):
     covered_call.fee = covered_call.fee - 1
     assert covered_call.immediate_gain() == 74.00
+
+def test_capped_gain(covered_call):
+    assert covered_call.capped_gain() == pytest.approx(2229.00,0.001)
 
 
