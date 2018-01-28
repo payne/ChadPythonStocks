388ad08e3feccb7d2bacd70c17f4b1164d9dd77a
Matt Payne <Payne@MattPayne.org>
2018-01-27 22:24:45 -0600

    lower fee means more profit.

--- cc/test_covered_call.py
+++ cc/test_covered_call.py
@@ -4,3 +4,5 @@ from covered_call import CoveredCall
 def test_example_two():
     cc = CoveredCall("LVS", 73.82, 77.44, 0.41, '2/23/2018', 2)
     assert cc.immediate_gain() == 73.00
+    cc.fee = cc.fee - 1
+    assert cc.immediate_gain() == 74.00
